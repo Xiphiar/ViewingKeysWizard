ec630a378f698027ae5deabed2f5394e1492879f
Xiphiar <me@xiphiar.com>
2021-09-15 22:17:48 -0400

    lcd url

--- src/KeplrStuff.tsx
+++ src/KeplrStuff.tsx
@@ -68,7 +68,7 @@ async function setupKeplr(
   const myAddress = accounts[0].address;
 
   const secretjs = new SigningCosmWasmClient(
-    "https://bridge-api-manager.azure-api.net/",
+    "https://lcd.scrt-archive.xiphiar.com/",
     myAddress,
     //@ts-ignore
     keplrOfflineSigner,
